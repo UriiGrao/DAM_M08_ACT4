76263c4a1d7e31ad2e2207e100ae6b1dd79d4319
UriiGrao <oriol.grao@iecisa.com>
2020-12-03 08:40:23 +0100

    created the menu page

--- app/src/main/java/com/Main/MainActivity.java
+++ app/src/main/java/com/Main/MainActivity.java
@@ -3,6 +3,8 @@ package com.Main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> spinnerDifficultArray = new ArrayAdapter<>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.spinnerDifficult));
+        spinnerDifficultArray.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        final Spinner spinnerDifficult = findViewById(R.id.spinnerDifficult);
+        spinnerDifficult.setAdapter(spinnerDifficultArray);
     }
 }
